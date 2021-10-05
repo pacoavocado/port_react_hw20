744070d66aad6d05638a3becb688e0a807e22e6f
Micah Conn <mcoxconn@gmail.com>
2021-10-04 23:41:06 -0500

    second commit? basic functionality/moving in old portfolio

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <img class="abtPic" src="./public/avatar (2).jpg" alt="Avatar"/>
+      <p id="description">
+                    He's a super cool dude, that likes to risk it all, and play it close 
+                    to the chest.  A time traveler to some and a prophet to others, he is 
+                    a traveler through space.
+                </p>
+    </div>
+  );
+}
