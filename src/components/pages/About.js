4f330b724babd7453d04a3f4a355f073e54e5699
Micah Conn <mcoxconn@gmail.com>
2021-10-04 23:44:53 -0500

    third commit git'n it good

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
       <h1>About Page</h1>
-      <img class="abtPic" src="./public/avatar (2).jpg" alt="Avatar"/>
+      <img class="abtPic" src="./public/avatar (2).jpg" alt="Avatar" />
       <p id="description">
-                    He's a super cool dude, that likes to risk it all, and play it close 
-                    to the chest.  A time traveler to some and a prophet to others, he is 
-                    a traveler through space.
-                </p>
+        He's a super cool dude, that likes to risk it all, and play it close to
+        the chest. A time traveler to some and a prophet to others, he is a
+        traveler through space.
+      </p>
+      <a href="https://drive.google.com/file/d/1L1aDQvehMLNeMmheiKAulxUJEk9oPo2a/view?usp=sharing">Resume</a>
     </div>
   );
 }
