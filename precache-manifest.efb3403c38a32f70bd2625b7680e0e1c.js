528a57fed918a4a76c82ba4d60e1b8a144af39f9
Micah Conn <mcoxconn@gmail.com>
2021-10-07 13:44:53 -0500

    Updates

---
+++ precache-manifest.efb3403c38a32f70bd2625b7680e0e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff41f8fc2b03dc34ec2ab169634a4a6",
+    "url": "/port_react_hw20/index.html"
+  },
+  {
+    "revision": "2e6fc144b473f06e75a2",
+    "url": "/port_react_hw20/static/css/2.de2856d6.chunk.css"
+  },
+  {
+    "revision": "2e6fc144b473f06e75a2",
+    "url": "/port_react_hw20/static/js/2.a018f486.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/port_react_hw20/static/js/2.a018f486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0405fa9bc819db067633",
+    "url": "/port_react_hw20/static/js/main.adf4eaa4.chunk.js"
+  },
+  {
+    "revision": "7a318372d9e42c4a9d64",
+    "url": "/port_react_hw20/static/js/runtime-main.e615f549.js"
+  }
+]);
